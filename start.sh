b033af55972808f9bf2801a1b7b2210eac1b5c28
Ajithdevops1e <ajithdocgym@gmail.com>
2025-05-16 10:31:47 +0530

    docker

--- start.sh
+++ start.sh
@@ -1,7 +1,20 @@
-
 #!/bin/bash
-service nginx start
-prometheus --config.file=/etc/prometheus/prometheus.yml &
+
+# Start NGINX in background
+echo "Starting NGINX..."
+nginx -g "daemon off;" &
+
+# Start Prometheus
+echo "Starting Prometheus..."
+prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/var/lib/prometheus &
+
+# Start Alertmanager
+echo "Starting Alertmanager..."
 alertmanager --config.file=/etc/alertmanager/alertmanager.yml &
-grafana-server --homepath=/usr/share/grafana &
+
+# Start Grafana
+echo "Starting Grafana..."
+grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/grafana.ini &
+
+# Keep container running
 tail -f /dev/null
